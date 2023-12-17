cfdd5053ea4f5e8a496b0c0feb71b6a19aac4746
darthReader <ddimitrov10@gmail.com>
2023-12-17 17:53:53 +0200

    front-end files

---
+++ public/src/app/menu/about/about.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  templateUrl: './about.component.html',
+  imports: [
+  ],
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
