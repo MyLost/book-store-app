52f8fc9696fd17c21945e3ecad28b045b5d40d96
darthReader <ddimitrov10@gmail.com>
2023-12-17 17:56:03 +0200

    front-end files

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { appRouts } from './app.routes';
 
 import { AppComponent } from './app.component';
@@ -6,12 +6,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { loginReducer } from './redux/reducers/LoginReducer';
 import { StoreModule } from '@ngrx/store';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NavComponent } from './menu/nav/nav.component';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [ AppComponent ],
   imports: [
+    NavComponent,
     RouterModule.forRoot(appRouts),
     RouterOutlet,
     BrowserModule,
@@ -19,19 +23,18 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     StoreModule.forRoot({
       loginData: loginReducer
     }),
-    NavComponent,
-    // TranslateModule.forRoot({
-    //   loader: {
-    //     provide: TranslateLoader,
-    //     useFactory: HttpLoaderFactory,
-    //     deps: [HttpClient]
-    //   }
-    // }),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
 
-// export function HttpLoaderFactory(http: HttpClient) {
-//   return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
-// }
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+}
