fdf15dfdcf35484d37e88995abdc0f89c2e8cf6e
darthReader <ddimitrov10@gmail.com>
2023-12-23 16:40:05 +0200

    frond-end files

--- backend-java/src/main/java/org/lost/backendjava/repositories/ApiTokenRepository.java
+++ backend-java/src/main/java/org/lost/backendjava/repositories/ApiTokenRepository.java
@@ -1,9 +1,22 @@
 package org.lost.backendjava.repositories;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.lost.backendjava.entities.ApiTokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApiTokenRepository extends JpaRepository<ApiTokenEntity, Long> {
+
+    @Query(value = """
+      select t from ApiTokenEntity t inner join UsersEntity u
+      on t.user.id = u.id
+      where u.id = :id and (t.expired = false or t.revoked = false)
+      """)
+    List<ApiTokenEntity> findAllValidTokenByUser(UUID id);
+
+    Optional<ApiTokenEntity> findByToken(String token);
 }
