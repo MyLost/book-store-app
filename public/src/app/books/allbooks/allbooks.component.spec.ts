d8de6e7687838c2ca45e5f45b37aa2aa1c8e8cdb
darthReader <ddimitrov10@gmail.com>
2023-12-23 16:39:30 +0200

    frond-end files

--- public/src/app/books/allbooks/allbooks.component.spec.ts
+++ public/src/app/books/allbooks/allbooks.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AllbooksComponent } from './allbooks.component';
+import { AllBooksComponent } from './allbooks.component';
 
 describe('AllbooksComponent', () => {
-  let component: AllbooksComponent;
-  let fixture: ComponentFixture<AllbooksComponent>;
+  let component: AllBooksComponent;
+  let fixture: ComponentFixture<AllBooksComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AllbooksComponent ]
+      declarations: [ AllBooksComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AllbooksComponent);
+    fixture = TestBed.createComponent(AllBooksComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
