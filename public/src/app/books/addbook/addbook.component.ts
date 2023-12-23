fbc95aee0fa0837e3555f4e3dc0085adfb40fe00
darthReader <ddimitrov10@gmail.com>
2023-12-23 16:39:20 +0200

    frond-end files

--- public/src/app/books/addbook/addbook.component.ts
+++ public/src/app/books/addbook/addbook.component.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import {MessageService} from 'primeng/api';
+import { MessageService, SelectItem } from 'primeng/api';
 import { BookService } from '../book.service';
-import { ToastModule } from "primeng/toast";
-import { FormsModule } from "@angular/forms";
-import { InputTextModule } from "primeng/inputtext";
+import { ToastModule } from 'primeng/toast';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
+import { DropdownModule } from 'primeng/dropdown';
+import { Genre } from '../common/Genre';
+import { BookInterface } from '../common/BookInterface';
+
+export interface BookRequest {
+  author: string;
+  title: string;
+  price: string;
+  genreId: string;
+  cover: string;
+  numberOfBooks: string;
+}
 
 @Component({
   selector: 'app-addbook',
@@ -13,60 +27,62 @@ import { InputTextModule } from "primeng/inputtext";
   imports: [
     ToastModule,
     FormsModule,
-    InputTextModule
+    InputTextModule,
+    ButtonModule,
+    PanelModule,
+    DropdownModule
   ],
-  providers: [MessageService, BookService]
+  providers: [ MessageService, BookService ]
 
 })
-export class AddbookComponent implements OnInit {
+export class AddBookComponent implements OnInit {
 
-  book = {
+  protected coverDropdownOptions: SelectItem[] = [
+    {label: 'Hard', value: 'HARD' },
+    {label: 'Soft', value: 'SOFT' }
+  ];
+
+  protected genres: Genre[];
+
+  protected bookRequestModel: BookRequest = {} as BookRequest;
+
+  protected success = {
     author: '',
     title: '',
     price: '',
-    kind: '',
+    genre: '',
     cover: '',
     numberOfBooks: ''
   };
-
-  success = {
-    author: '',
-    title: '',
-    price: '',
-    kind: '',
-    cover: '',
-    numberOfBooks: ''
-  };
-  keyMessage = '';
-  flag: any;
+  protected keyMessage = '';
+  protected flag: any;
 
   constructor(private messageService: MessageService, private bookService: BookService) { }
 
   ngOnInit() {
-
+    this.bookService.getGenres().subscribe(result => this.genres = result);
   }
 
   checkInput(event: any, type: any) {
 
-    if (this.book.author === '' && type.name === 'author') {
-      this.messageService.add({severity: 'info', summary: 'Input Message  ', detail: 'Author is required ', sticky: true});
+    if (this.bookRequestModel.author === '' && type.name === 'author') {
+      this.messageService.add({ key: 'book', severity: 'error', summary: 'Error', detail: 'Author is required '});
     }
-    if (this.book.title === '' && type.name === 'title')  {
+    if (this.bookRequestModel.title === '' && type.name === 'title')  {
       // tslint:disable-next-line:max-line-length
-      this.messageService.add({severity: 'info', summary: 'Input Message  ', detail: 'Title is required ', sticky: true});
-
+      this.messageService.add({key: 'book', severity: 'error', summary: 'Error', detail: 'Title is required '});
     }
-    if (this.book.price === '' && type.name === 'price' ) {
-      this.messageService.add({severity: 'info', summary: 'Input Message  ', detail: 'Price is required ', sticky: true});
+    if (this.bookRequestModel.price === '' && type.name === 'price' ) {
+      this.messageService.add({key: 'book', severity: 'error', summary: 'Error', detail: 'Price is required '});
     }
-    if (this.book.kind === '' && type.name === 'kind') {
-      this.messageService.add({severity: 'info', summary: 'Input Message  ', detail: 'Kind is required ', sticky: true});
+    if (this.bookRequestModel.genreId === '' && type.name === 'kind') {
+      this.messageService.add({key: 'book', severity: 'error', summary: 'Error', detail: 'Kind is required '});
     }
-    if (this.book.cover === '' && type.name === 'cover') {
-      this.messageService.add({severity: 'info', summary: 'Input Message  ', detail: 'Cover is required ', sticky: true});
+    if (this.bookRequestModel.cover === '' && type.name === 'cover') {
+      this.messageService.add({key: 'book', severity: 'error', summary: 'Error', detail: 'Cover is required '});
     }
-    if (this.book.numberOfBooks === ''  && type.name === 'numberOfBooks') {
-      this.messageService.add({severity: 'info', summary: 'Input Message  ', detail: 'Number Of Books is required ', sticky: true});
+    if (this.bookRequestModel.numberOfBooks === ''  && type.name === 'numberOfBooks') {
+      this.messageService.add({key: 'book', severity: 'error', summary: 'Error', detail: 'Number Of Books is required '});
     }
 
 
@@ -77,7 +93,7 @@ export class AddbookComponent implements OnInit {
     if (event.key === 'Backspace') {
     }
 
-    if (!this.book.author.match('<') && type.name === 'author') {
+    if (!this.bookRequestModel.author.match('<') && type.name === 'author') {
       console.log('Wrong symbol in author checkInput function');
       this.success.author = '';
       this.keyMessage = 'author';
@@ -85,35 +101,35 @@ export class AddbookComponent implements OnInit {
       this.flag = false;
     }
 
-    if (!this.book.title.match('<') && type.name === 'title' ) {
+    if (!this.bookRequestModel.title.match('<') && type.name === 'title' ) {
       this.success.title = '';
       console.log('title empty');
       this.keyMessage = 'title';
       this.flag = false;
     }
 
-    if (!this.book.price.match('<') && type.name === 'price' ) {
+    if (!this.bookRequestModel.price.match('<') && type.name === 'price' ) {
       this.success.price = '';
       console.log('price empty');
       this.keyMessage = 'price';
       this.flag = false;
     }
 
-    if (!this.book.kind.match('<') && type.name === 'kind' ) {
-      this.success.kind = '';
+    if (!this.bookRequestModel.genreId.match('<') && type.name === 'kind' ) {
+      this.success.genre = '';
       console.log('kind empty');
       this.keyMessage = 'kind';
       this.flag = false;
     }
 
-    if (!this.book.cover.match('<') && type.name === 'cover' ) {
+    if (!this.bookRequestModel.cover.match('<') && type.name === 'cover' ) {
       this.success.cover = '';
       console.log('cover empty');
       this.keyMessage = 'cover';
       this.flag = false;
     }
 
-    if (!this.book.numberOfBooks.match('<') && type.name === 'numberOfBooks' ) {
+    if (!this.bookRequestModel.numberOfBooks.match('<') && type.name === 'numberOfBooks' ) {
       this.success.numberOfBooks = '';
       console.log('numberOfBooks empty');
       this.keyMessage = 'numberOfBooks';
@@ -128,37 +144,37 @@ export class AddbookComponent implements OnInit {
       console.log('right author');
       this.success.author = 'author';
       // tslint:disable-next-line:max-line-length
-      this.messageService.add({key: this.keyMessage, severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
+      this.messageService.add({key: 'book', severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
       }
 
       if (this.success.title === '' && type.name === 'title') {
       this.success.title = 'title';
       // tslint:disable-next-line:max-line-length
-      this.messageService.add({key: this.keyMessage, severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
+      this.messageService.add({key: 'book', severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
       }
 
       if (this.success.price === '' && type.name === 'price') {
       this.success.price = 'price';
       // tslint:disable-next-line:max-line-length
-      this.messageService.add({key: this.keyMessage, severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
+      this.messageService.add({key: 'book', severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
       }
 
-      if (this.success.kind === '' && type.name === 'kind') {
-      this.success.kind = 'kind';
+      if (this.success.genre === '' && type.name === 'kind') {
+      this.success.genre = 'kind';
       // tslint:disable-next-line:max-line-length
-      this.messageService.add({key: this.keyMessage, severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
+      this.messageService.add({key: 'book', severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
       }
 
       if (this.success.cover === '' && type.name === 'cover') {
       this.success.cover = 'cover';
       // tslint:disable-next-line:max-line-length
-      this.messageService.add({key: this.keyMessage, severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
+      this.messageService.add({key: 'book', severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
       }
 
       if (this.success.numberOfBooks === '' && type.name === 'numberOfBooks') {
       this.success.numberOfBooks = 'numberOfBooks';
       // tslint:disable-next-line:max-line-length
-      this.messageService.add({key: this.keyMessage, severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
+      this.messageService.add({key: 'book', severity: 'warn', summary: 'Input Message  ', detail: 'Wrong symbol in ' + this.keyMessage + ' input: ' + event.key, sticky: true});
       }
 
 
@@ -172,33 +188,33 @@ export class AddbookComponent implements OnInit {
   }
 
   cheker(type: any) {
-    if ( type.name === 'author' && this.book.author.match('<')) {
+    if ( type.name === 'author' && this.bookRequestModel.author.match('<')) {
       this.keyMessage = 'author';
       this.flag = true;
       console.log('match author');
 
     }
-    if ( type.name === 'title' && this.book.title.match('<')) {
+    if ( type.name === 'title' && this.bookRequestModel.title.match('<')) {
       console.log('match title');
       this.keyMessage = 'title';
       this.flag = true;
     }
-    if (type.name === 'cover' && this.book.cover.match('<')) {
+    if (type.name === 'cover' && this.bookRequestModel.cover.match('<')) {
       this.keyMessage = 'cover';
       this.flag = true;
     }
 
-    if (type.name === 'kind' && this.book.kind.match('<')) {
+    if (type.name === 'kind' && this.bookRequestModel.genreId.match('<')) {
       this.keyMessage = 'kind';
       this.flag = true;
     }
 
-    if (type.name === 'price' && this.book.price.match('<')) {
+    if (type.name === 'price' && this.bookRequestModel.price.match('<')) {
       this.keyMessage = 'price';
       this.flag = true;
     }
 
-    if (type.name === 'numberOfBooks' && this.book.numberOfBooks.match('<')) {
+    if (type.name === 'numberOfBooks' && this.bookRequestModel.numberOfBooks.match('<')) {
       this.keyMessage = 'numberOfBooks';
       this.flag = true;
     }
@@ -207,15 +223,13 @@ export class AddbookComponent implements OnInit {
   }
 
   addBook() {
-    this.bookService.addBook(this.book).subscribe((data: any) => {
-      if (data.success) {
-        console.log(data);
-        this.messageService.add({severity: 'success', summary: 'Service Message', detail: 'Book added correctly'});
+    this.bookService.addBook(this.bookRequestModel).subscribe((result: BookInterface) => {
+      if (result) {
+        this.bookRequestModel = {} as BookRequest;
+        this.messageService.add({key: 'book', severity: 'success', summary: 'Service Message', detail: 'Book added correctly'});
       } else {
-        console.log(data.message);
-        this.messageService.add({severity: 'error', summary: 'Service Message', detail: 'Book not added correctly'});
+        this.messageService.add({key: 'book', severity: 'error', summary: 'Service Message', detail: 'Book not added correctly'});
       }
     });
   }
-
 }
