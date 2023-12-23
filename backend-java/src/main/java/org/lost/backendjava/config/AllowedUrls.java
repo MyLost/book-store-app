2fc6e97b49280d59cf9ea4d10bffb6a3b909dde2
darthReader <ddimitrov10@gmail.com>
2023-12-23 16:39:43 +0200

    frond-end files

--- backend-java/src/main/java/org/lost/backendjava/config/AllowedUrls.java
+++ backend-java/src/main/java/org/lost/backendjava/config/AllowedUrls.java
@@ -6,7 +6,17 @@ public class AllowedUrls {
 
     private static final String[] allowedUrls = {
         "/api/v1/auth/**",
-        "/api/v1/test/**"
+        "/api/v1/test/**",
+        "/v2/api-docs",
+        "/v3/api-docs",
+        "/v3/api-docs/**",
+        "/swagger-resources",
+        "/swagger-resources/**",
+        "/configuration/ui",
+        "/configuration/security",
+        "/swagger-ui/**",
+        "/webjars/**",
+        "/swagger-ui.html"
     };
 
     @Getter
