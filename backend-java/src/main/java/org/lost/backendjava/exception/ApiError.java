19f269ccdc37715fa7eeb650c235055ae4edaafb
darthReader <ddimitrov10@gmail.com>
2023-12-23 16:39:48 +0200

    frond-end files

---
+++ backend-java/src/main/java/org/lost/backendjava/exception/ApiError.java
@@ -0,0 +1,13 @@
+package org.lost.backendjava.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiError {
+
+    private String message;
+}
