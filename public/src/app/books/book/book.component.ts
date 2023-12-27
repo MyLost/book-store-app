0231c5f702b1856619b3f3da88160b0d4f369f78
darthReader <ddimitrov10@gmail.com>
2023-12-27 09:25:27 +0200

    frond-end files

--- public/src/app/books/book/book.component.ts
+++ public/src/app/books/book/book.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { TabViewModule } from 'primeng/tabview';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TabView, TabViewModule } from 'primeng/tabview';
 import { AllBooksComponent } from '../allbooks/allbooks.component';
 import { AddBookComponent } from '../addbook/addbook.component';
-import { EditbookComponent } from '../editbook/editbook.component';
-import { FindbookComponent } from '../findbook/findbook.component';
-import { DeletebookComponent } from '../deletebook/deletebook.component';
+import { EditBookComponent } from '../editbook/editbook.component';
+import { Router, RouterOutlet } from '@angular/router';
+import { PanelModule } from 'primeng/panel';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-book',
@@ -14,18 +17,27 @@ import { DeletebookComponent } from '../deletebook/deletebook.component';
     TabViewModule,
     AllBooksComponent,
     AddBookComponent,
-    EditbookComponent,
-    FindbookComponent,
-    DeletebookComponent,
+    EditBookComponent,
+    RouterOutlet,
+    PanelModule,
+    MenubarModule,
+    ToastModule,
   ],
   styleUrls: ['./book.component.css']
 })
 
 export class BookComponent implements OnInit {
 
-  constructor() { }
+  protected items: MenuItem[];
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
-  }
 
+    this.items = [
+      { label: 'All' , routerLink: 'all' },
+      { label: 'Add' , routerLink: 'add' },
+      { label: 'Find', routerLink: 'find'},
+    ];
+  }
 }
