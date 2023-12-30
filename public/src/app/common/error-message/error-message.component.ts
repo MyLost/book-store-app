44781e9f2aa909da5adb5b829ab778894e268ab9
darthReader <ddimitrov10@gmail.com>
2023-12-30 10:27:40 +0200

    frond-end files

---
+++ public/src/app/common/error-message/error-message.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { AbstractControl, FormControl, NgForm } from '@angular/forms';
+import { ValidationPipe } from '../pipes/validation.pipe';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-error-message',
+  standalone: true,
+  imports: [
+    ValidationPipe,
+    NgIf
+  ],
+  templateUrl: './error-message.component.html',
+  host: { style: 'color: red;' },
+
+})
+export class ErrorMessageComponent {
+
+  @Input() control!: FormControl | AbstractControl;
+  @Input() errorMessages!: Object;
+
+  constructor(public ngForm: NgForm) {
+    console.log(ngForm);
+  }
+
+}
