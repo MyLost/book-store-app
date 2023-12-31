41c150e18458d0b6c18b46e0651db6cb031769a1
darthReader <ddimitrov10@gmail.com>
2023-12-31 14:30:08 +0200

    frond-end files

---
+++ backend-java/src/main/java/org/lost/backendjava/models/LoginModel.java
@@ -0,0 +1,12 @@
+package org.lost.backendjava.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginModel {
+
+    private String username;
+    private String password;
+}
