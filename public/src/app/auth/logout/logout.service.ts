27f80ae42640b1dd4d8e3d615b947a35a0625a3f
darthReader <ddimitrov10@gmail.com>
2024-01-26 07:20:16 +0200

    frond-end files

---
+++ public/src/app/auth/logout/logout.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BaseService } from '../../base.service';
+import { HttpClient } from '@angular/common/http';
+import { shareReplay } from 'rxjs';
+import { logout } from '../../common/Utils';
+
+@Injectable()
+export class LogoutService extends BaseService<any> {
+
+  private logoutUrl = this.baseUrl + 'auth/logout';
+
+  constructor( private http: HttpClient ) {
+    super();
+  }
+
+  logout() {
+    localStorage.clear();
+    logout.emit(true);
+    const o = this.http.post(this.logoutUrl, {}).pipe(shareReplay());
+    o.subscribe();
+    return o;
+  }
+}
