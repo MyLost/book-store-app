bb1e84910f699917cda83c000cad70b60001c473
darthReader <ddimitrov10@gmail.com>
2024-01-27 11:52:09 +0200

    frond-end files

---
+++ public/src/app/common/translate/translate.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-translate',
+  standalone: true,
+  templateUrl: './translate.component.html',
+  styleUrls: ['./translate.component.css']
+})
+export class TranslateComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
