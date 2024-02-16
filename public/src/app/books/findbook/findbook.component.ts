a3963b9edd0aaacd9b1d65755e76e322c86f2202
darthReader <ddimitrov10@gmail.com>
2024-02-16 12:37:13 +0200

    frond-end files

---
+++ public/src/app/books/findbook/findbook.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ToastModule } from "primeng/toast";
+import { MessageModule } from "primeng/message";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from "primeng/button";
+import { BookInterface } from "../common/BookInterface";
+import { BookService } from "../book.service";
+import { TableModule } from "primeng/table";
+
+
+@Component({
+  selector: 'app-findbook',
+  standalone: true,
+  templateUrl: './findbook.component.html',
+  styleUrls: ['./findbook.component.css'],
+  imports: [
+    ToastModule,
+    ReactiveFormsModule,
+    MessageModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule
+  ],
+  providers: []
+})
+export class FinBookComponent implements OnInit {
+
+  protected findForm: FormGroup;
+  protected books: BookInterface[];
+
+
+  constructor(
+    private bookSvc: BookService
+  ) {
+    this.findForm = new FormGroup ({
+      author : new FormControl (''),
+      title : new FormControl (''),
+    });
+  }
+
+  ngOnInit() {}
+
+  findBook (findForm: FormGroup) {
+    this.bookSvc.findBook(findForm.value).subscribe(books => this.books = books);
+  }
+}
---
+++ public/src/app/user/edit-user-component/edit-user-component.component.ts
@@ -0,0 +1,95 @@
+import { Component, OnInit } from '@angular/core';
+import { editUser, dashboard } from '../../common/Utils';
+import { DialogModule } from 'primeng/dialog';
+import { FileUploadModule } from 'primeng/fileupload';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { UserInterface } from '../User';
+import { AuthService } from '../../auth/auth.service';
+import { PanelModule } from 'primeng/panel';
+import { UserService } from '../user-service.service';
+import { MessageService } from "primeng/api";
+import { LOGIN_USER } from "../../redux/actions/loginActions";
+import { LoginState } from "../../redux/store/login.state";
+import { Store } from "@ngrx/store";
+
+
+export interface UserEditRequest {
+  id: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface UserEditResponse {
+  username: string;
+}
+
+@Component({
+  selector: 'app-edit-user-component',
+  standalone: true,
+  templateUrl: './edit-user-component.component.html',
+  imports: [
+    DialogModule,
+    FileUploadModule,
+    FormsModule,
+    InputTextModule,
+    PanelModule
+  ],
+  styleUrls: ['./edit-user-component.component.css']
+})
+export class EditUserComponentComponent implements OnInit {
+
+  protected display: boolean;
+  protected user: UserEditRequest;
+
+  constructor(
+    private authSvc: AuthService,
+    private userSvc: UserService,
+    private messageSvc: MessageService,
+    private store: Store<LoginState>
+    ) {
+    editUser.subscribe(show => {
+      this.display = show;
+    });
+  }
+
+  ngOnInit() {
+    this.user = {} as UserEditRequest;
+    this.authSvc.user?.subscribe((user: UserInterface) =>  {
+      this.user.username = user.username;
+      this.user.id = user.id;
+    });
+  }
+
+  edit() {
+    this.userSvc.editUser(this.user).subscribe( (result: UserEditResponse) => {
+      if (result) {
+        const user = null;
+        this.authSvc.user.subscribe(user => { user = user; });
+        this.messageSvc.add({
+          summary: 'Success',
+          severity: 'success',
+          detail: 'User was updated successfully'
+        });
+        this.store.dispatch({
+          type: LOGIN_USER,
+          payload: {
+            logged: true,
+            access_token: this.authSvc.authorizationToken,
+            refresh_token: this.authSvc.refreshToken,
+            authenticated: true,
+            dashboard: { display: true },
+            user: user
+          }
+        });
+        editUser.emit(true);
+      }
+    });
+  }
+
+  hide() {
+    this.display = false;
+    dashboard.emit(true);
+  }
+}
