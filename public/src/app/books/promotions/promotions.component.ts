1041e79192363dbd3fa681614c2599b9c394c0f1
darthReader <ddimitrov10@gmail.com>
2024-02-17 09:08:42 +0200

    frond-end files

---
+++ public/src/app/books/promotions/promotions.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { BookService } from '../book.service';
+import { BookInterface } from '../common/BookInterface';
+import { ImageModule } from "primeng/image";
+
+
+@Component({
+  selector: 'app-list-store',
+  standalone: true,
+  templateUrl: './promotions.component.html',
+  imports: [
+    ButtonModule,
+    RippleModule,
+    JsonPipe,
+    ImageModule
+],
+  styleUrls: ['./promotions.component.css'],
+  providers: [ BookService ]
+})
+export class PromotionsComponent implements OnInit {
+
+  protected books: BookInterface[];
+
+  constructor(private bookSvc: BookService) { }
+
+  ngOnInit() {
+    this.bookSvc.getPromotions().subscribe((data: BookInterface[]) => this.books = data);
+  }
+}
