10190c3263acc40cc5b333c5d66ba2cd9844951f
darthReader <ddimitrov10@gmail.com>
2024-02-27 12:16:32 +0200

    backend-end files

---
+++ backend-java/src/main/java/org/lost/backendjava/exception/ApiErrorResponse.java
@@ -0,0 +1,34 @@
+package org.lost.backendjava.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatusCode;
+
+/*
+    {
+     "timestamp": 1688920909,
+     "status": 400,
+     "error": "Bad Request",
+     "exception": "org.springframework.http.converter.HttpMessageNotReadableException",
+     "message": "JSON parse error: Unrecognized token ....",
+     "path": "/albums"
+    }
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class ApiErrorResponse {
+
+    private Long timestamp;
+    private String message;
+    private HttpStatusCode httpStatusCode;
+    private String error;
+    private String path;
+
+    public ApiErrorResponse(String message) {
+        this.message = message;
+    }
+}
