f719190d95ddff10531e7fc9b8de32680511d1be
darthReader <ddimitrov10@gmail.com>
2024-03-02 08:59:25 +0200

    backend-end files

---
+++ backend-java/src/main/java/org/lost/backendjava/exception/BaseExceptionHandler.java
@@ -0,0 +1,29 @@
+package org.lost.backendjava.exception;
+
+import java.sql.Timestamp;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+    @ExceptionHandler({ Exception.class })
+    public final ResponseEntity<ApiErrorResponse> handleException(Exception ex, WebRequest request) {
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        return new ResponseEntity<>(
+            ApiErrorResponse.builder()
+            .error(ex.getMessage())
+            .message(ex.getMessage())
+            .httpStatusCode(HttpStatus.NOT_FOUND)
+            .path(request.getContextPath())
+            .timestamp(System.currentTimeMillis())
+            .build(), headers, status);
+    }
+}
