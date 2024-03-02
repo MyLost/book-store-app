23643c2849e4159957138f1ea7f597a30b6fce3e
darthReader <ddimitrov10@gmail.com>
2024-03-02 09:51:23 +0200

    backend-end files

---
+++ backend-java/src/main/java/org/lost/backendjava/entities/BookGenreEntity.java
@@ -0,0 +1,26 @@
+package org.lost.backendjava.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "book_genres")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookGenreEntity extends BaseEntity {
+
+    @Column
+    private String name;
+
+    @Column
+    private String descriptions;
+}
