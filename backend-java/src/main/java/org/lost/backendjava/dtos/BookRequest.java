3d63eb3a02703eb42d72b9b6029a81b3f4f7264c
darthReader <ddimitrov10@gmail.com>
2024-03-03 06:40:20 +0200

    backend-end files

---
+++ backend-java/src/main/java/org/lost/backendjava/dtos/BookRequest.java
@@ -0,0 +1,17 @@
+package org.lost.backendjava.dtos;
+
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookRequest {
+
+    private String author;
+    private String title;
+    private BigDecimal price;
+    private Long genreId;
+    private String cover;
+    private Integer numberOfBooks;
+}
