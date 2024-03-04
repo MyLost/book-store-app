ad94a10fd93e303e4486f59277cce50594359bfa
darthReader <ddimitrov10@gmail.com>
2024-03-04 11:01:15 +0200

    backend-end files

---
+++ backend-java/src/main/java/org/lost/backendjava/params/BookSearchParams.java
@@ -0,0 +1,14 @@
+package org.lost.backendjava.params;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+//This is basic search, but enough for this application
+public class BookSearchParams {
+
+    private String author;
+    private String title;
+
+}
