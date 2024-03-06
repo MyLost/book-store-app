f33723475b96184669f20cc00d930fdd44270d75
darthReader <ddimitrov10@gmail.com>
2024-03-06 08:49:38 +0200

    backend-end files

---
+++ backend-java/src/main/java/org/lost/backendjava/entities/CommentEntity.java
@@ -0,0 +1,30 @@
+package org.lost.backendjava.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="comments")
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentEntity extends BaseEntity {
+
+    @Column
+    private String authorName;
+
+    @Column
+    private String content;
+
+    @ManyToOne(targetEntity= ArticleEntity.class)
+    @JoinColumn(nullable=false)
+    private ArticleEntity article;
+}
