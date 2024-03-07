5cedfe09d7dabf08b7b649a1f96d8eb83198dbb9
darthReader <ddimitrov10@gmail.com>
2024-03-07 09:57:03 +0200

    backend-end files

---
+++ backend-java/src/main/java/org/lost/backendjava/repositories/CommentRepository.java
@@ -0,0 +1,9 @@
+package org.lost.backendjava.repositories;
+
+import org.lost.backendjava.entities.CommentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+}
