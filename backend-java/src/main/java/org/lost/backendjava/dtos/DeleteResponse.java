2fdd942524cbeb6d3d7677955db967f5707b244d
darthReader <ddimitrov10@gmail.com>
2024-03-07 09:58:33 +0200

    backend-end files

---
+++ backend-java/src/main/java/org/lost/backendjava/dtos/DeleteResponse.java
@@ -0,0 +1,18 @@
+package org.lost.backendjava.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeleteResponse {
+
+    private boolean success;
+    private String message;
+}
