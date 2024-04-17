02015e8039f9eec1c3a52b5710064548a38bcdf6
midas <ddimitrov10@gmail.com>
2024-04-17 12:45:01 +0300

    backend-end files

--- backend-java/src/main/java/org/softuni/repositories/BookRepository.java
+++ backend-java/src/main/java/org/softuni/repositories/BookRepository.java
@@ -1,7 +1,7 @@
-package org.lost.backendjava.repositories;
+package org.softuni.repositories;
 
 import java.util.List;
-import org.lost.backendjava.entities.BookEntity;
+import org.softuni.entities.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
