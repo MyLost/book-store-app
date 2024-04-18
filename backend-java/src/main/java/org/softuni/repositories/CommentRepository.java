a693d04e34013490191b69d37cf44fcb02254e89
midas <ddimitrov10@gmail.com>
2024-04-18 08:42:59 +0300

    backend-end files

--- backend-java/src/main/java/org/softuni/repositories/CommentRepository.java
+++ backend-java/src/main/java/org/softuni/repositories/CommentRepository.java
@@ -1,6 +1,6 @@
-package org.lost.backendjava.repositories;
+package org.softuni.repositories;
 
-import org.lost.backendjava.entities.CommentEntity;
+import org.softuni.entities.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
