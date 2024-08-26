6c09a905227246d95bb2593fee395c85f021c62c
midas <ddimitrov10@gmail.com>
2024-08-26 09:57:24 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/BackendJavaApplication.java
@@ -0,0 +1,13 @@
+package org.npd21tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaApplication.class, args);
+	}
+
+}
