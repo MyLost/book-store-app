da5b25f6bc9a97be8331927baf0247a9f8967a24
midas <ddimitrov10@gmail.com>
2024-09-01 22:22:48 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/dtos/BookGenreResponse.java
@@ -0,0 +1,14 @@
+package org.npd21tech.dtos;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookGenreResponse {
+
+    private Long id;
+    private String name;
+    private String descriptions;
+}
