132f78752c759bab29706dd4413f155e3750ccdc
midas <ddimitrov10@gmail.com>
2024-09-02 08:49:50 +0300

    WIP

---
+++ backend-java/src/main/java/org/npd21tech/dtos/BookRequest.java
@@ -0,0 +1,17 @@
+package org.npd21tech.dtos;
+
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookRequest {
+
+    private String author;
+    private String title;
+    private BigDecimal price;
+    private Long genreId;
+    private String cover;
+    private Integer numberOfBooks;
+}
