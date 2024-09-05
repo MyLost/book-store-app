e31de3820fe71d4a049289c39ebd34c64c89110f
midas <ddimitrov10@gmail.com>
2024-09-05 16:50:56 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/entities/BaseEntity.java
@@ -0,0 +1,20 @@
+package org.npd21tech.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.Persistable;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
