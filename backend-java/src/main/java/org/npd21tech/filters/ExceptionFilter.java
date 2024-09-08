c10b411cdd6de51783e99aacf50da9ba6ee06062
midas <ddimitrov10@gmail.com>
2024-09-08 09:15:33 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/filters/ExceptionFilter.java
@@ -0,0 +1,23 @@
+package org.npd21tech.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@Order(1)
+public class ExceptionFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
