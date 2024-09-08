f629709e7aed3e0ac27bf81fbe2701433f83f814
midas <ddimitrov10@gmail.com>
2024-09-08 09:15:45 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/models/AuthenticationResponseModel.java
@@ -0,0 +1,17 @@
+package org.npd21tech.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponseModel {
+
+    private String accessToken;
+    private String refreshToken;
+}
