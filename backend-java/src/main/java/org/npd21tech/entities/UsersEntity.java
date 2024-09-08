852f9bd376fa70662fa72c96d43bdc569936b57d
midas <ddimitrov10@gmail.com>
2024-09-08 09:15:03 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/entities/UsersEntity.java
@@ -0,0 +1,87 @@
+package org.npd21tech.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+
+import org.npd21tech.enums.Roles;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.annotation.Validated;
+
+@Getter
+@Setter
+@Entity
+@Validated
+@Table(name="users")
+public class UsersEntity implements UserDetails {
+
+    @Id
+    private UUID id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private LocalDate bornOn;
+
+    @Column
+    private String email;
+
+    @Column
+    private boolean isActive;
+
+    @Column
+    private Roles role;
+
+    @Column
+    private String phoneNumber;
+
+    @OneToMany(targetEntity=ApiTokenEntity.class, mappedBy="user")
+    private List<ApiTokenEntity> apiTokens;
+
+    @Column
+    private String google_email;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
