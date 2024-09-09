649c492e17b08116bcd5e3162172c889971d10e4
midas <ddimitrov10@gmail.com>
2024-09-09 09:43:28 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/repositories/GenreRepository.java
@@ -0,0 +1,9 @@
+package org.npd21tech.repositories;
+
+import org.npd21tech.entities.BookGenreEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<BookGenreEntity, Long> {
+}
