7bc53ded37380f6fca468ca6e95fc53be8dd1488
midas <ddimitrov10@gmail.com>
2024-09-09 09:43:31 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package org.npd21tech.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.npd21tech.entities.UsersEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UsersEntity, UUID> {
+
+    Optional<UsersEntity> findByUsername(String username);
+    Page<UsersEntity> findAll(Pageable pageable);
+
+}
