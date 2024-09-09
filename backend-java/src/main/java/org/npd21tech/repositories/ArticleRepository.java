47e7717bddf7ba1ccfe1bbbfd3795c82054259b1
midas <ddimitrov10@gmail.com>
2024-09-09 09:43:16 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/repositories/ArticleRepository.java
@@ -0,0 +1,9 @@
+package org.npd21tech.repositories;
+
+import org.npd21tech.entities.ArticleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
+}
