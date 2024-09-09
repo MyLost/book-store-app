169c7318e6ab784d6c6c65a59664b45f06edb266
midas <ddimitrov10@gmail.com>
2024-09-09 09:43:24 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/repositories/CommentRepository.java
@@ -0,0 +1,9 @@
+package org.npd21tech.repositories;
+
+import org.npd21tech.entities.CommentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+}
