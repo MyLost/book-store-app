b0940ff800b71733733befebe3c7c68c8815f8ca
midas <ddimitrov10@gmail.com>
2024-09-09 09:42:52 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/models/LoginModel.java
@@ -0,0 +1,12 @@
+package org.npd21tech.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginModel {
+
+    private String username;
+    private String password;
+}
