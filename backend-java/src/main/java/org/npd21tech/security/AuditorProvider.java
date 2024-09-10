0a4df94e58188bf4b68309751cf53d1f07235c51
midas <ddimitrov10@gmail.com>
2024-09-10 11:43:05 +0300

    wip

---
+++ backend-java/src/main/java/org/npd21tech/security/AuditorProvider.java
@@ -0,0 +1,28 @@
+package org.npd21tech.security;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.npd21tech.entities.UsersEntity;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class AuditorProvider implements AuditorAware<UUID> {
+
+    @Override
+    public Optional<UUID> getCurrentAuditor() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null ||
+            !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken
+        ) {
+            return Optional.empty();
+        }
+
+        UsersEntity user = (UsersEntity) authentication.getPrincipal();
+        return Optional.ofNullable(user.getId());
+    }
+}
