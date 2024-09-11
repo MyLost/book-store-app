7c0f7be3c2da150458582bc8e49e0b4c8b142087
midas <ddimitrov10@gmail.com>
2024-09-11 11:53:47 +0300

    wip

--- public/src/app/books/addbook/addbook.component.spec.ts
+++ public/src/app/books/addbook/addbook.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {  waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddBookComponent } from './addbook.component';
 
@@ -6,7 +6,7 @@ describe('AddbookComponent', () => {
   let component: AddBookComponent;
   let fixture: ComponentFixture<AddBookComponent>;
 
-  beforeEach(async(() => {
+  beforeEach( waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AddBookComponent ]
     })
