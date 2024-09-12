471e0d5a583cb9cd1a17b0592d5343bbafc37255
midas <ddimitrov10@gmail.com>
2024-09-12 19:04:47 +0300

    wip

--- public/src/app/app.routes.ts
+++ public/src/app/app.routes.ts
@@ -1,40 +1,20 @@
-import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot, Routes, UrlTree } from '@angular/router';
+import { Routes } from '@angular/router';
 
 import { HomeComponent } from './menu/home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './menu/register/register.component';
 import { AboutComponent } from './menu/about/about.component';
-import { PromotionsComponent } from './books/promotions/promotions.component';
-import { StoreComponent } from './books/store/store.book.component';
-import { LogoutComponent } from './auth/logout/logout.component';
-import { MailComponent } from './common/mail/mail.component';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { PageNotFoundComponent } from './common/page-not-found/page-not-found.component';
-import { EmployeesComponent } from './Employees/employees.component';
-import { Observable } from 'rxjs';
-import { AuthService } from './auth/auth.service';
-import { inject } from '@angular/core';
 
-export const appGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree>
-  | Promise<boolean | UrlTree> | boolean | UrlTree => {
-    const authSvc = inject(AuthService);
-    return authSvc.isLoggedIn();
-};
 
 export const appRouts: Routes = [
-  { path: '', canActivateChild: [appGuard], children: [
-    { path: 'books', loadChildren: () => import('./books/book.router').then(routes => routes.bookRouts)},
-    { path: 'storeBook' , component: StoreComponent},
-    { path: 'listBook' , component: PromotionsComponent},
-    { path: 'dashboard' , component: DashboardComponent},
-    { path: 'logout' , component: LogoutComponent},
-    { path: 'mail', component: MailComponent},
-    { path: 'employees', component: EmployeesComponent},
-  ]},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
   { path: 'home', component: HomeComponent},
   { path: 'about' , component: AboutComponent},
   { path: 'register', component: RegisterComponent},
+  { path: 'books',  loadChildren: () => import('./books/book.router').then(routes => routes.bookRouts)},
   { path: '**', component: PageNotFoundComponent }
 ];
+
 
