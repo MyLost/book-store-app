18f094538d776ac9d00589443a210953246b2465
midas <ddimitrov10@gmail.com>
2024-09-12 19:04:39 +0300

    wip

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,21 +1,29 @@
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, importProvidersFrom, NgModule } from '@angular/core';
 import { appRouts } from './app.routes';
 
 import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration, withI18nSupport } from '@angular/platform-browser';
 import { loginReducer } from './redux/reducers/LoginReducer';
 import { StoreModule } from '@ngrx/store';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHandler,
+  provideHttpClient,
+  withInterceptorsFromDi
+} from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NavComponent } from './menu/nav/nav.component';
 import { httpInterceptorProviders } from './Interceptors/interceptors';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
-import { ProgressSpinnerModule } from "primeng/progressspinner";
-import { CustomErrorHandler } from "./common/Utils";
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { CustomErrorHandler } from './common/Utils';
+import { MessagesModule } from "primeng/messages";
+import {environment} from "../environments/environment";
+import {TieredMenuModule} from "primeng/tieredmenu";
 
 @NgModule({
   declarations: [ AppComponent ],
@@ -32,15 +40,19 @@ import { CustomErrorHandler } from "./common/Utils";
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
+        deps: [ HttpClient ]
+      },
+      defaultLanguage: environment.defaultLanguage
     }),
     ToastModule,
     ProgressSpinnerModule,
+    MessagesModule,
+    TieredMenuModule,
   ],
   bootstrap: [ AppComponent ],
   providers: [ MessageService, httpInterceptorProviders,
-    { provide: ErrorHandler, useClass: CustomErrorHandler }
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: ErrorHandler, useClass: CustomErrorHandler },
   ]
 })
 export class AppModule { }
