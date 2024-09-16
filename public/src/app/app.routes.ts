06f30fd0a9fca5e4ea68dcb05b14595f3c82209e
midas <ddimitrov10@gmail.com>
2024-09-16 09:17:56 +0300

    wip

--- public/src/app/app.routes.ts
+++ public/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './menu/register/register.component';
 import { AboutComponent } from './menu/about/about.component';
 import { PageNotFoundComponent } from './common/page-not-found/page-not-found.component';
+import {LogoutComponent} from "./auth/logout/logout.component";
+import {appGuard} from "./auth/guards";
+import {BookComponent} from "./books/book/book.component";
 
 
 export const appRouts: Routes = [
@@ -13,8 +16,9 @@ export const appRouts: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'about' , component: AboutComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'books',  loadChildren: () => import('./books/book.router').then(routes => routes.bookRouts)},
+  { path: 'logout', canActivate:[appGuard], component: LogoutComponent},
+  { path: 'books', component: BookComponent},
+  { path: 'books/genre/:id', component: BookComponent },
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.router').then(routes => routes.dashboardRouts)},
   { path: '**', component: PageNotFoundComponent }
 ];
-
-
