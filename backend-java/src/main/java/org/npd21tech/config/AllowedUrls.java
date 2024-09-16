0c6c9f9b5b21fc4a96cbbb81b7239d17fe233539
midas <ddimitrov10@gmail.com>
2024-09-16 09:17:43 +0300

    wip

--- backend-java/src/main/java/org/npd21tech/config/AllowedUrls.java
+++ backend-java/src/main/java/org/npd21tech/config/AllowedUrls.java
@@ -6,7 +6,9 @@ public class AllowedUrls {
 
     private static final String[] allowedUrls = {
         "/api/v1/books/most-popular",
+        "/api/v1/books/genre/{genreId}",
         "/api/v1/books/paged-list",
+        "/api/v1/books/genres",
         "/api/v1/auth/**",
         "/api/v1/test/**",
         "/v2/api-docs",
@@ -20,7 +22,6 @@ public class AllowedUrls {
         "/webjars/**",
         "/swagger-ui.html"
     };
-
     @Getter
     private static final String logOutUrl = "/api/v1/auth/logout";
 
