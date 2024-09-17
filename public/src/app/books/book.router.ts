6a3f59fe8e3f7d38ef65969812f1881f642fa7a2
midas <ddimitrov10@gmail.com>
2024-09-17 09:50:10 +0300

    wip

--- public/src/app/books/book.router.ts
+++ public/src/app/books/book.router.ts
@@ -1,18 +1,32 @@
-import { ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
-import { BookComponent } from './book/book.component';
+import {ActivatedRouteSnapshot, RouterStateSnapshot, Routes} from '@angular/router';
 import { AddBookComponent } from './addbook/addbook.component';
 import { EditBookComponent } from './editbook/editbook.component';
 import { FinBookComponent } from './findbook/findbook.component';
 import { AllBooksComponent } from './allbooks/allbooks.component';
 import { BookResolver } from './book-resolver';
 import { inject } from '@angular/core';
+import {ManageComponent} from "./manage/manage.component";
+import {appGuard} from "../auth/guards";
+import {PromotionsComponent} from "./promotions/promotions.component";
+import {StoreComponent} from "./store/store.book.component";
+import {MailComponent} from "../common/mail/mail.component";
+import {EmployeesComponent} from "../Employees/employees.component";
+import {BookComponent} from "./book/book.component";
 
 export const bookRouts: Routes = [
-  { path: '', component: BookComponent, children: [
+  { path: '', component: ManageComponent, canActivateChild: [appGuard], children: [
     { path: 'all', component: AllBooksComponent},
+    { path: 'manage', component: ManageComponent },
     { path: 'add', component: AddBookComponent},
-    { path: 'edit/:id', component: EditBookComponent,
-      resolve: {'book': (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(BookResolver).resolve(route, state)}},
+    { path: 'edit/:id', component: EditBookComponent, resolve: {
+      'book': (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(BookResolver).resolve(route, state)
+      }
+    },
     { path: 'find', component: FinBookComponent},
-  ]},
+    { path: 'promotions' , component: PromotionsComponent },
+    { path: 'store' , component: StoreComponent},
+    { path: 'mail', component: MailComponent},
+    { path: 'employees', component: EmployeesComponent}
+    ]},
 ];
+
