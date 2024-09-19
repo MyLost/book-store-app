24fcaab2acde5309d96ae8b29b78f6e53fa3a90d
midas <ddimitrov10@gmail.com>
2024-09-19 15:41:19 +0300

    wip

--- public/src/app/books/common/BookInterface.ts
+++ public/src/app/books/common/BookInterface.ts
@@ -1,10 +1,11 @@
 export interface BookInterface {
-
-   id: string;
-   author: string;
-   title: string;
-   price: string;
-   genre: any;
-   cover: string;
-   numberOfBooks: string;
+  id: string;
+  author: string;
+  title: string;
+  price: string;
+  genre: any;
+  cover: string;
+  numberOfBooks: string;
+  inventoryStatus: string;
+  coverImage: any[]
 }
