866c429e722bfcf0a5482bbe17662b75a738ea19
midas <ddimitrov10@gmail.com>
2024-09-23 22:03:14 +0300

    wip

--- backend-java/src/main/java/org/npd21tech/repositories/BookRepository.java
+++ backend-java/src/main/java/org/npd21tech/repositories/BookRepository.java
@@ -3,6 +3,8 @@ package org.npd21tech.repositories;
 import java.util.List;
 
 import org.npd21tech.entities.BookEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,8 @@ public interface BookRepository extends JpaRepository<BookEntity, Long> {
     List<BookEntity> findAllByPromotionEquals(boolean promotion);
 
     List<BookEntity> findAllByAuthorEqualsAndTitleEquals(String author, String title);
+
+    Page<BookEntity> findAll(Pageable pageable);
+
+    Page<BookEntity> findAllByGenreId(Pageable pageable, Long genreId);
 }
