558bf8da247da8fd2f35b37f581a9b6922c99462
midas <ddimitrov10@gmail.com>
2024-09-30 09:55:47 +0300

    wip

--- backend-java/src/main/java/org/softuni/repositories/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package org.softuni.repositories;
-
-import org.softuni.entities.CommentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
-}
