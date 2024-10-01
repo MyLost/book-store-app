2ad3a48a213aeab28603f92dac86aefa5f18ef6e
midas <ddimitrov10@gmail.com>
2024-10-01 10:10:47 +0300

    wip

--- public/src/app/user/edit-user-component/edit-user-component.component.spec.ts
+++ public/src/app/user/edit-user-component/edit-user-component.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {  waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditUserComponentComponent } from './edit-user-component.component';
 
@@ -6,7 +6,7 @@ describe('EditUserComponentComponent', () => {
   let component: EditUserComponentComponent;
   let fixture: ComponentFixture<EditUserComponentComponent>;
 
-  beforeEach(async(() => {
+  beforeEach( waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EditUserComponentComponent ]
     })
