698368c9efd10fc0fe3dad788591e52a8e827bb0
midas <ddimitrov10@gmail.com>
2024-10-03 09:43:15 +0300

    wip

--- backend-java/src/main/java/org/softuni/repositories/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package org.softuni.repositories;
-
-import org.softuni.entities.BookGenreEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenreRepository extends JpaRepository<BookGenreEntity, Long> {
-}
