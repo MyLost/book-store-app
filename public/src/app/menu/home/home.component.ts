4c6f44073c7e5515200def14338171c81dd5f2a0
midas <ddimitrov10@gmail.com>
2024-10-04 09:49:10 +0300

    wip

--- public/src/app/menu/home/home.component.ts
+++ public/src/app/menu/home/home.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit} from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
+import {TranslateModule} from "@ngx-translate/core";
+import {CarouselModule} from "primeng/carousel";
+import {NgOptimizedImage} from "@angular/common";
+import {PanelModule} from "primeng/panel";
+import {BookService} from "../../books/book.service";
+import {BookInterface} from "../../books/common/BookInterface";
+import {environment} from "../../../environments/environment";
+import {TagModule} from "primeng/tag";
+import {CardModule} from "primeng/card";
 
 
 @Component({
@@ -10,14 +19,40 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./home.component.css'],
   imports: [
     ButtonModule,
-    RouterLink
+    RouterLink,
+    TranslateModule,
+    CarouselModule,
+    NgOptimizedImage,
+    PanelModule,
+    TagModule,
+    CardModule
   ],
-  providers: []
-
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {}
+  protected books: BookInterface[];
 
-  ngOnInit() {}
+  protected responsiveOptions: any[] | undefined;
+
+  protected readonly environment = environment;
+
+  constructor(private bookSvc: BookService ) {}
+
+  ngOnInit() {
+    this.bookSvc.getMostPopular().subscribe((data: BookInterface[])=> this.books = data);
+  }
+
+  protected getSeverity(status: string) {
+    switch (status) {
+      case 'INSTOCK':
+        return 'success';
+      case 'LOWSTOCK':
+        return 'info';
+      case 'HIGHSTOCK':
+        return 'warning';
+      case 'OUTOFSTOCK':
+        return 'danger';
+    }
+  }
+
 }
