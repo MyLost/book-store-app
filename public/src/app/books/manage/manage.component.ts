1155e26a5c15617a1bf12719abee15b6d3d6baf8
midas <ddimitrov10@gmail.com>
2024-12-12 09:47:14 +0200

    wip

---
+++ public/src/app/books/manage/manage.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {MenubarModule} from "primeng/menubar";
+import {PanelModule} from "primeng/panel";
+import {Router, RouterOutlet} from "@angular/router";
+import {ToastModule} from "primeng/toast";
+import {MenuItem} from "primeng/api";
+
+@Component({
+  selector: 'app-manage',
+  standalone: true,
+    imports: [
+        MenubarModule,
+        PanelModule,
+        RouterOutlet,
+        ToastModule
+    ],
+  templateUrl: './manage.component.html',
+  styleUrl: './manage.component.scss'
+})
+export class ManageComponent implements OnInit {
+
+  protected items: MenuItem[];
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.items = [
+      { label: 'All' , routerLink: 'all' },
+      { label: 'Add' , routerLink: 'add' },
+      { label: 'Find', routerLink: 'find'},
+    ];
+  }
+}
