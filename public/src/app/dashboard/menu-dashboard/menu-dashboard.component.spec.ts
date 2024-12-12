a8ff1cd7e99871231fba7a7db83bb1f932c46f21
midas <ddimitrov10@gmail.com>
2024-12-12 09:47:17 +0200

    wip

--- public/src/app/dashboard/menu-dashboard/menu-dashboard.component.spec.ts
+++ public/src/app/dashboard/menu-dashboard/menu-dashboard.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {  waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuDashboardComponent } from './menu-dashboard.component';
 
@@ -6,7 +6,7 @@ describe('MenuDashboardComponent', () => {
   let component: MenuDashboardComponent;
   let fixture: ComponentFixture<MenuDashboardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach( waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MenuDashboardComponent ]
     })
