53323e6037f1b46a265e26f63d632b4110a1a6af
midas <ddimitrov10@gmail.com>
2024-12-22 17:05:19 +0200

    wip

--- public/src/app/menu/nav/nav.service.ts
+++ public/src/app/menu/nav/nav.service.ts
@@ -1,33 +1,50 @@
-import { Injectable } from '@angular/core';
-import { AuthService } from '../../auth/auth.service';
-import { dashboard } from '../../common/Utils';
+import {Injectable} from '@angular/core';
+import {AuthService} from '../../auth/auth.service';
+import {dashboard} from '../../common/Utils';
+import {TranslateService} from "@ngx-translate/core";
+import {lastValueFrom} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavService {
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private translateService: TranslateService
+    ) {}
 
-  public menuItems() {
+  private get menuItems() {
     return [
       { label: 'Home',  icon: 'pi pi-home',  routerLink: ['/home']},
       { label: 'About', icon: 'pi pi-info',  routerLink: ['/about'] },
       { label: 'Login', icon: 'pi pi-sign-in', routerLink: ['/login'], visible: !this.authService.isLoggedIn() },
       { label: 'Register', icon: 'pi pi-user-plus', routerLink: ['/register'], visible: !this.authService.isLoggedIn() },
-      {
-        label: 'Dashboard', icon: 'pi pi-plus', visible: this.authService.isLoggedIn(),
-        command: () => {
-          dashboard.emit(true); }
-      },
+      { label: 'Dashboard', icon: 'pi pi-plus', visible: this.authService.isLoggedIn(), routerLink: ['/dashboard'] },
+      { label: 'Books', icon: 'pi pi-book', visible: this.authService.isLoggedIn(), routerLink: ['/books'] },
       {
         label: 'Logout',  icon: 'pi pi-sign-out',  routerLink: ['/logout'],
         visible: this.authService.isLoggedIn(),
         command: () => { }
       },
-      { label: 'Books', icon: 'pi pi-book',  routerLink: ['/books'], visible: this.authService.isLoggedIn() },
-      { label: 'Promotion', icon: 'pi pi-list', routerLink: ['/listBook'], visible: this.authService.isLoggedIn() },
-      { label: 'Store', icon: 'pi pi-database', routerLink: ['/storeBook'], visible: this.authService.isLoggedIn() }
     ];
+  }
+
+  private get menuItemLabels() {
+    return this.menuItems.map(item => item.label);
+  }
+
+  private getTranslateMenuItemLabels() {
+    let promise = lastValueFrom(this.translateService.get(this.menuItemLabels));
+    promise.then();
+    return promise;
+  }
+
+  async loadMenuItems() {
+    const translations = await this.getTranslateMenuItemLabels();
+    return this.menuItems.map(item => {
+      item.label = translations[item.label];
+      return item;
+    });
   }
 }
