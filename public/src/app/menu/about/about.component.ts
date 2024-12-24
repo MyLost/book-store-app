99a69c14226ed1c5abe0445fd3a43d6e469de2ff
midas <ddimitrov10@gmail.com>
2024-12-24 11:42:52 +0200

    wip

--- public/src/app/menu/about/about.component.ts
+++ public/src/app/menu/about/about.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  templateUrl: './about.component.html',
-  imports: [
-  ],
-  styleUrls: ['./about.component.css']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    imports: [],
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
