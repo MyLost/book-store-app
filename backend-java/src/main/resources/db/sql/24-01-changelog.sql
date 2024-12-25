ad0dfbe20028d722b45dfab8e3e4ad64f826abf3
midas <ddimitrov10@gmail.com>
2024-12-25 16:16:09 +0200

    wip

--- backend-java/src/main/resources/db/sql/24-01-changelog.sql
+++ backend-java/src/main/resources/db/sql/24-01-changelog.sql
@@ -1,10 +1,13 @@
 -- liquibase formatted sql
 
+-- changeset midas:1735030554920-1
+-- DROP DATABASE IF EXISTS bookStore
+
 -- changeset midas:1735030554921-1
-CREATE SEQUENCE revinfo_seq INCREMENT BY 50 START WITH 1;
+CREATE SEQUENCE if not exists revinfo_seq INCREMENT BY 50 START WITH 1;
 
 -- changeset midas:1735030554921-2
-CREATE TABLE revinfo
+CREATE TABLE if not exists revinfo
 (
     rev      INT NOT NULL,
     revtstmp BIGINT NULL,
@@ -12,7 +15,7 @@ CREATE TABLE revinfo
 );
 
 -- changeset midas:1735030555058-1
-CREATE TABLE book_store.api_token
+CREATE TABLE if not exists bookStore.api_token
 (
     id         BIGINT AUTO_INCREMENT NOT NULL,
     token      VARCHAR(255) NULL,
@@ -25,7 +28,7 @@ CREATE TABLE book_store.api_token
 );
 
 -- changeset midas:1735030555058-2
-CREATE TABLE book_store.articles
+CREATE TABLE if not exists bookStore.articles
 (
     id           BIGINT AUTO_INCREMENT NOT NULL,
     title        VARCHAR(255) NULL,
@@ -36,7 +39,7 @@ CREATE TABLE book_store.articles
 );
 
 -- changeset midas:1735030555058-3
-CREATE TABLE book_store.book_genres
+CREATE TABLE if not exists bookStore.book_genres
 (
     id           BIGINT AUTO_INCREMENT NOT NULL,
     name         VARCHAR(255) NULL,
@@ -45,7 +48,7 @@ CREATE TABLE book_store.book_genres
 );
 
 -- changeset midas:1735030555058-4
-CREATE TABLE book_store.books
+CREATE TABLE if not exists bookStore.books
 (
     id               BIGINT AUTO_INCREMENT NOT NULL,
     author           VARCHAR(255) NULL,
@@ -63,7 +66,7 @@ CREATE TABLE book_store.books
 );
 
 -- changeset midas:1735030555058-5
-CREATE TABLE book_store.comments
+CREATE TABLE if not exists bookStore.comments
 (
     id          BIGINT AUTO_INCREMENT NOT NULL,
     author_name VARCHAR(255) NULL,
@@ -73,7 +76,7 @@ CREATE TABLE book_store.comments
 );
 
 -- changeset midas:1735030555058-6
-CREATE TABLE book_store.users
+CREATE TABLE if not exists bookStore.users
 (
     id           BINARY(16) NOT NULL,
     first_name   VARCHAR(255) NULL,
@@ -90,14 +93,20 @@ CREATE TABLE book_store.users
 );
 
 -- changeset midas:1735030555058-7
-ALTER TABLE book_store.api_token
-    ADD CONSTRAINT FK_API_TOKEN_ON_USER FOREIGN KEY (user_id) REFERENCES book_store.users (id);
+ALTER TABLE bookStore.api_token
+    ADD CONSTRAINT FK_API_TOKEN_ON_USER FOREIGN KEY IF NOT EXISTS (user_id) REFERENCES bookStore.users (id);
 
 -- changeset midas:1735030555058-8
-ALTER TABLE book_store.books
-    ADD CONSTRAINT FK_BOOKS_ON_GENRE FOREIGN KEY (genre_id) REFERENCES book_store.book_genres (id);
+ALTER TABLE bookStore.books
+    ADD CONSTRAINT FK_BOOKS_ON_GENRE FOREIGN KEY IF NOT EXISTS (genre_id) REFERENCES bookStore.book_genres (id);
 
 -- changeset midas:1735030555058-9
-ALTER TABLE book_store.comments
-    ADD CONSTRAINT FK_COMMENTS_ON_ARTICLE FOREIGN KEY (article_id) REFERENCES book_store.articles (id);
+ALTER TABLE bookStore.comments
+    ADD CONSTRAINT FK_COMMENTS_ON_ARTICLE FOREIGN KEY IF NOT EXISTS (article_id) REFERENCES bookStore.articles (id);
+
+-- changeset midas:1735030555058-10
+SET FOREIGN_KEY_CHECKS = 0;
+truncate bookStore.books;
+truncate bookStore.book_genres;
+SET FOREIGN_KEY_CHECKS = 1;
 
