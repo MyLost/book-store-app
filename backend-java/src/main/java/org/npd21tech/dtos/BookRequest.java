8cf53b31fd32abca48f80e791617b4d2feee28a8
midas <ddimitrov10@gmail.com>
2024-12-29 12:15:59 +0200

    wip

--- backend-java/src/main/java/org/npd21tech/dtos/BookRequest.java
+++ backend-java/src/main/java/org/npd21tech/dtos/BookRequest.java
@@ -3,6 +3,7 @@ package org.npd21tech.dtos;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
@@ -14,4 +15,12 @@ public class BookRequest {
     private Long genreId;
     private String cover;
     private Integer numberOfBooks;
+    private ImageRequest coverImage;
+
+    @Getter
+    @Setter
+    public static class ImageRequest {
+        private String image;
+        private String name;
+    }
 }
